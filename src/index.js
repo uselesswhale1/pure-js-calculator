72dff2491e09fae428d6d53707f06398fe5327da
Даня Шевченко <54992950+uselesswhale1@users.noreply.github.com>
2021-12-05 01:25:33 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Calculator from './components/calculator'
 ReactDOM.render(
   <React.StrictMode>
   <Calculator/>
+  <h2>Я люблю Валюшика</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
